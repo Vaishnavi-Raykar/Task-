8e676c2c0619d4ce2dbd3b463316365a7d24b2a6
Vaishnavi-Raykar <vaishnaviraykar990@gmail.com>
2024-03-08 23:38:55 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Loginform from '@/components/Loginform'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Loginform />
+    </div>
+  )
+}
+
+export default page
